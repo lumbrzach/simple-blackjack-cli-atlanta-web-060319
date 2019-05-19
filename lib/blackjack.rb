8b6c47b7f1260385d4525dabeb4ea5b1fd0d1e94
Zach Parker <parkerz410@gmail.com>
2019-05-19 15:02:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,10 +32,10 @@ end
 
 def hit? (card_total)
   prompt_user
-  get_user_input
-  if get_user_input == 's'
+  user_input = get_user_input
+  if user_input == 's'
     card_total
-  elsif get_user_input == 'h'
+  elsif user_input == 'h'
     deal_card
     card_total = card_total + deal_card
   else
